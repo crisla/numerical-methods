62b13ce9a7998b96a181c1287db72d877c384fe0
Cristina <s1254888@sms.ed.ac.uk>
2016-10-28 14:09:19 +0100

    More cosmetic improvements
    - Paths to images in markdown cells fixed (except the trajectories one)
    - Gridsize of grpahs fixed
    - Cpuple typo corrections

--- labs/lab-2/exercise_4c.py
+++ labs/lab-2/exercise_4c.py
@@ -1,5 +1,5 @@
 # suppose there is a 50% increase in delta
 plt.figure(figsize=(8,6))
-model.plot_phase_diagram(gridmax=100, N=1000, arrows=True, param='delta', 
+model.plot_phase_diagram(gridmax=30, N=1000, arrows=True, param='delta', 
                          shock=1.5, reset=True)
 plt.show()
